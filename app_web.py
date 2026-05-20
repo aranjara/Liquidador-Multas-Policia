43b0c5eb85166ec1380d9012177f7593fc510f51
aranjara <aranjara@gmail.com>
2026-05-20 09:43:51 -0500

    deploy: configurar app para produccion en servidor VPS (Gunicorn + Nginx)

--- app_web.py
+++ app_web.py
@@ -2,4 +2,4 @@ from backend.app import app, create_app
 
 
 if __name__ == "__main__":
-    create_app().run(host="127.0.0.1", port=5000, debug=True)
+    create_app().run(host="0.0.0.0", port=5000, debug=False)
